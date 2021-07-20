b1c3b5df49e4353c3254d8175b13c5cb27e6bc83
Mayank Gupta <mayank.pkgupta@gmail.com>
2021-07-20 11:58:23 +0530

    Added search functionality

--- js/all.js
+++ js/all.js
@@ -1,49 +1,142 @@
-const dataDiv = document.querySelector('.data-div')
+const dataDiv = document.querySelector(".data-div");
 
-var employees = db.collection('employees')
+var employees = db.collection("employees");
 
 //Render All Employees to Table
 const renderEmployees = (doc, sr) => {
-  let tr = document.createElement('tr')
-  let count = document.createElement('td')
-  let name = document.createElement('td')
-  let designation = document.createElement('td')
-  let department = document.createElement('td')
+  let tr = document.createElement("tr");
+  let count = document.createElement("td");
+  let name = document.createElement("td");
+  let designation = document.createElement("td");
+  let department = document.createElement("td");
 
-  tr.setAttribute('data-id', doc.id)
-  count.textContent = sr
-  name.textContent = doc.data().name
-  designation.textContent = doc.data().designation
-  department.textContent = doc.data().department
+  tr.setAttribute("data-id", doc.id);
+  count.textContent = sr;
+  name.textContent = doc.data().name;
+  designation.textContent = doc.data().designation;
+  department.textContent = doc.data().department;
 
-  tr.appendChild(count)
-  tr.appendChild(name)
-  tr.appendChild(designation)
-  tr.appendChild(department)
+  tr.appendChild(count);
+  tr.appendChild(name);
+  tr.appendChild(designation);
+  tr.appendChild(department);
 
-  dataDiv.append(tr)
-}
+  dataDiv.append(tr);
+};
 
 const getAllEmployeesData = async () => {
-  count = 0
-  const data = await employees.get()
+  count = 0;
+  const data = await employees.get();
   data.forEach((doc) => {
-    count = count + 1
-    renderEmployees(doc, count)
-  })
-}
+    count = count + 1;
+    renderEmployees(doc, count);
+  });
+};
 
-getAllEmployeesData()
+getAllEmployeesData();
 
 //Get Query Employees
 const getQueryEmployeesData = async () => {
-  count = 0
+  count = 0;
   const data = await employees
-    .where('designation', 'array-contains-any', ['Registrar'])
-    .get()
+    .where("designation", "array-contains-any", ["Registrar"])
+    .get();
   data.forEach((doc) => {
-    count = count + 1
-    renderEmployees(doc, count)
-  })
-}
+    count = count + 1;
+    renderEmployees(doc, count);
+  });
+};
 //getQueryEmployeesData()
+
+function myFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("searchByName");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+//Search Functionality
+
+// Search By Name
+function searchByNameFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("searchByName");
+  filter = input.value.toLowerCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toLowerCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+// Search By Designation
+function searchByDesignationFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("searchByDesignation");
+  filter = input.value.toLowerCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[2];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toLowerCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+// Search By Department
+function searchByDepartmentFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("searchByDepartment");
+  filter = input.value.toLowerCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[3];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toLowerCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
--- js/search.js
+++ js/search.js
@@ -1,100 +1,100 @@
-const dataDiv = document.querySelector('.data-div')
-const searchByName = document.querySelector('#searchByName')
-const searchByDesignation = document.querySelector('#searchByDesignation')
-const searchByDepartment = document.querySelector('#searchByDepartment')
-const searchForm = document.querySelector('#searchForm')
+// const dataDiv = document.querySelector('.data-div')
+// const searchByName = document.querySelector('#searchByName')
+// const searchByDesignation = document.querySelector('#searchByDesignation')
+// const searchByDepartment = document.querySelector('#searchByDepartment')
+// const searchForm = document.querySelector('#searchForm')
 
-var employees = db.collection('employees')
+// var employees = db.collection('employees')
 
-//Render Employees to Table
-const renderEmployees = (doc, sr) => {
-  let tr = document.createElement('tr')
-  let count = document.createElement('td')
-  let name = document.createElement('td')
-  let designation = document.createElement('td')
-  let department = document.createElement('td')
+// //Render Employees to Table
+// const renderEmployees = (doc, sr) => {
+//   let tr = document.createElement('tr')
+//   let count = document.createElement('td')
+//   let name = document.createElement('td')
+//   let designation = document.createElement('td')
+//   let department = document.createElement('td')
 
-  tr.setAttribute('data-id', doc.id)
-  count.textContent = sr
-  name.textContent = doc.data().name
-  designation.textContent = doc.data().designation
-  department.textContent = doc.data().department
+//   tr.setAttribute('data-id', doc.id)
+//   count.textContent = sr
+//   name.textContent = doc.data().name
+//   designation.textContent = doc.data().designation
+//   department.textContent = doc.data().department
 
-  tr.appendChild(count)
-  tr.appendChild(name)
-  tr.appendChild(designation)
-  tr.appendChild(department)
+//   tr.appendChild(count)
+//   tr.appendChild(name)
+//   tr.appendChild(designation)
+//   tr.appendChild(department)
 
-  dataDiv.append(tr)
-}
+//   dataDiv.append(tr)
+// }
 
-const getAllEmployeesData = async () => {
-  count = 0
-  const data = await employees.get()
-  data.forEach((doc) => {
-    count = count + 1
-    renderEmployees(doc, count)
-  })
-}
+// const getAllEmployeesData = async () => {
+//   count = 0
+//   const data = await employees.get()
+//   data.forEach((doc) => {
+//     count = count + 1
+//     renderEmployees(doc, count)
+//   })
+// }
 
-//getAllEmployeesData()
+// //getAllEmployeesData()
 
-//Get Query Employees
-const getDataByName = async (name) => {
-  count = 0
-  const data = await employees.where('name', '==', name).get()
-  data.forEach((doc) => {
-    count = count + 1
-    renderEmployees(doc, count)
-  })
-}
-const getDataByDesignation = async (designation) => {
-  count = 0
-  const data = await employees.where('designation', '==', designation).get()
-  data.forEach((doc) => {
-    count = count + 1
-    renderEmployees(doc, count)
-  })
-}
-const getDataByDepartment = async (department) => {
-  count = 0
-  const data = await employees.where('department', '==', department).get()
-  data.forEach((doc) => {
-    count = count + 1
-    renderEmployees(doc, count)
-  })
-}
+// //Get Query Employees
+// const getDataByName = async (name) => {
+//   count = 0
+//   const data = await employees.where('name', '==', name).get()
+//   data.forEach((doc) => {
+//     count = count + 1
+//     renderEmployees(doc, count)
+//   })
+// }
+// const getDataByDesignation = async (designation) => {
+//   count = 0
+//   const data = await employees.where('designation', '==', designation).get()
+//   data.forEach((doc) => {
+//     count = count + 1
+//     renderEmployees(doc, count)
+//   })
+// }
+// const getDataByDepartment = async (department) => {
+//   count = 0
+//   const data = await employees.where('department', '==', department).get()
+//   data.forEach((doc) => {
+//     count = count + 1
+//     renderEmployees(doc, count)
+//   })
+// }
 
-//Form Submit
-searchForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-  dataDiv.textContent = ''
+// //Form Submit
+// searchForm.addEventListener('submit', (e) => {
+//   e.preventDefault()
+//   dataDiv.textContent = ''
 
-  if (
-    searchByName.value == '' &&
-    searchByDesignation.value == '' &&
-    searchByDepartment.value == ''
-  ) {
-    alert('All the Search Boxes are Empty, Please Enter some Search Query!')
-  } else if (
-    searchByName.value !== '' &&
-    searchByDesignation.value == '' &&
-    searchByDepartment.value == ''
-  ) {
-    getDataByName(searchByName.value.toLowerCase())
-  } else if (
-    searchByName.value == '' &&
-    searchByDesignation.value !== '' &&
-    searchByDepartment.value == ''
-  ) {
-    getDataByDesignation(searchByDesignation.value.toLowerCase())
-  } else if (
-    searchByName.value == '' &&
-    searchByDesignation.value == '' &&
-    searchByDepartment.value !== ''
-  ) {
-    getDataByDepartment(searchByDepartment.value.toLowerCase())
-  } else {
-    alert('Under Progress')
-  }
-})
+//   if (
+//     searchByName.value == '' &&
+//     searchByDesignation.value == '' &&
+//     searchByDepartment.value == ''
+//   ) {
+//     alert('All the Search Boxes are Empty, Please Enter some Search Query!')
+//   } else if (
+//     searchByName.value !== '' &&
+//     searchByDesignation.value == '' &&
+//     searchByDepartment.value == ''
+//   ) {
+//     getDataByName(searchByName.value.toLowerCase())
+//   } else if (
+//     searchByName.value == '' &&
+//     searchByDesignation.value !== '' &&
+//     searchByDepartment.value == ''
+//   ) {
+//     getDataByDesignation(searchByDesignation.value.toLowerCase())
+//   } else if (
+//     searchByName.value == '' &&
+//     searchByDesignation.value == '' &&
+//     searchByDepartment.value !== ''
+//   ) {
+//     getDataByDepartment(searchByDepartment.value.toLowerCase())
+//   } else {
+//     alert('Under Progress')
+//   }
+// })
